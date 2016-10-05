e6e3592ffd6a0fa5014a8d2fc390e362f1c783b7
Danil Zaviryukha <logolevel@gmail.com>
2016-10-05 12:04:43 +0300

    Added header

---
+++ app/js/common.js
@@ -0,0 +1,23 @@
+$(function() {
+
+/*accordion*/
+  $(function() {
+
+    $("#lang").msDropdown();
+
+  });
+
+
+/*carousel*/
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:0,
+      responsive:{
+          0:{
+              items:1
+          }
+      }
+  })
+
+
+});
