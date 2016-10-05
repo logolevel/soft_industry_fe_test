df5e8776dbff3347043bf67ff653e6b9af7b94f9
Danil Zaviryukha <logolevel@gmail.com>
2016-10-06 00:27:53 +0300

    Added slider and popular section

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,34 @@
 $(function() {
 
-/*accordion*/
-  $(function() {
+/*drop select with img*/
 
-    $("#lang").msDropdown();
-
-  });
-
+  $("#lang").msDropdown();
 
 /*carousel*/
   $('.owl-carousel').owlCarousel({
       loop:true,
+      nav:true,
       margin:0,
       responsive:{
           0:{
               items:1
           }
-      }
+      },
+    navText:""
   })
+/*search nav*/
+$('.owl-prev').addClass('icon-arrow-left');
+$('.owl-next').addClass('icon-arrow-right');
+
+/*like rating*/
+$('.like-rating ul li').on('click', function() {
+  var selectedCssClass = 'selected';
+  var $this = $(this);
+  $this.siblings('.' + selectedCssClass).removeClass(selectedCssClass);
+  $this
+    .addClass(selectedCssClass)
+    .parent().addClass('vote-cast');
+});
 
 
 });
