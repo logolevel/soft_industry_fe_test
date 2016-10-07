bea1a5c2070d2db63dc8d144d9d595f6a712583c
Danil Zaviryukha <logolevel@gmail.com>
2016-10-07 23:42:45 +0300

    edited readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # soft_industry_fe_test
 Furniture template
+<h2><a href="https://logolevel.github.io/projects/soft_industry_fe_test/index.html">VIEW DEMO</a></h2>
---
+++ dist/js/common.js
@@ -0,0 +1,52 @@
+$(function() {
+
+/*drop select with img*/
+
+  $("#lang").msDropdown();
+
+/*carousel*/
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      nav:true,
+      margin:0,
+      responsive:{
+          0:{
+              items:1
+          }
+      },
+    navText:""
+  })
+/*search nav*/
+$('.owl-prev').addClass('icon-arrow-left');
+$('.owl-next').addClass('icon-arrow-right');
+
+/*like rating*/
+$('.like-rating ul li').on('click', function() {
+  var selectedCssClass = 'selected';
+  var $this = $(this);
+  $this.siblings('.' + selectedCssClass).removeClass(selectedCssClass);
+  $this
+    .addClass(selectedCssClass)
+    .parent().addClass('vote-cast');
+});
+
+/*menu toggle*/
+
+  function blueaseMenu() {
+    var $trigger = $('.menu__trigger'),
+        $menu = $('.menu__list');
+
+    $trigger.click(function(){
+      $(this).next($menu).slideToggle();
+    });
+
+    $(window).resize(function() {
+      if ( $(window).width() > 880 ) {
+        $menu.removeAttr('style');
+      } 
+    });
+  }
+  blueaseMenu();
+
+
+});
